7c7c30362cf72a98834200b53eb380053ce97252
Sinitsa <sinitsammi@gmail.com>
2024-12-02 13:37:18 +0300

    HomeworkPictures

---
+++ Pictures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int picturesInAlbum = 52;
+            int picturesInRow = 3;
+            int rowsCount = picturesInAlbum / picturesInRow;
+            int extraPicturesCount = picturesInAlbum % picturesInRow;
+
+            Console.WriteLine($" { rowsCount } рядов(а) по {picturesInRow} картинки, {extraPicturesCount} картинки(а) сверх меры ");
+        }
+    }
+}
